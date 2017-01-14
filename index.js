c0d31e8a55dee1dbf6f41cd23bd8785ee64cdbde
郑俊鑫 <18810544291@163.com>
2017-01-14 16:26:02 +0800

    add function

---
+++ index.js
@@ -0,0 +1,10 @@
+export default function block (millisecond) {
+    if (millisecond <= 0) return
+
+    let now = performance.now()
+    const end = now + millisecond
+
+    while(now <= end) {
+        now = performance.now()
+    }
+}
