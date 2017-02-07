8a96ee1e9804108c7381e0f8ccb27a032d529f19
郑俊鑫 <18810544291@163.com>
2017-02-07 10:40:35 +0800

    change let to var to support uglifyjs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
     function block (millisecond) {
         if (millisecond <= 0) return
 
-        let now = performance.now()
+        var now = performance.now()
         const end = now + millisecond
 
         while(now <= end) {
